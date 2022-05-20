4b6b60a3465d97a0cca10002ec5fa8fa399a40fc
Joshua <70519085+UrFingPoor@users.noreply.github.com>
2022-05-20 00:51:48 -0400

    Error Handling

--- DownloadAPI/Installer.cs
+++ DownloadAPI/Installer.cs
@@ -9,64 +9,77 @@ namespace Spaceware
     class DownloadAPI
     {
         /// <summary>
-        /// // Method: InitDownload();
-        /// // Description: Initializes the installer process. 
-        /// // Usage: DownloadWorker.InitDownload();
+        /// Method: InitDownload();
+        /// Description: Initializes the installer process. 
+        /// Usage: DownloadWorker.InitDownload();
+        /// Return Values: 0 = Folder Not Found | 1 = Lastest Version | 2 = ERROR | 3 = Valid Response
         /// </summary>
-        public bool InitDownload()
+        public int InitDownload()
         {
-            using (var client = new WebClient { Headers = { "Accept: text/html, application/xhtml+xml, */*", "User-Agent: Mozilla/9.0 (compatible; MSIE 9.0; Windows NT 6.1; WOW64; SpacewareAPI/9.0)" } })
+            try
             {
-                if (VRChatFolder() == string.Empty | !vrchatPath.Contains("VRChat")) return false;
-                ClientVersion = FileVersionInfo.GetVersionInfo($"{vrchatPath}\\Area51\\DLL\\Area51.dll").FileVersion;
-                ServerVersion = client.DownloadString($"{APILink}version");
-                zipPath = $"{Path.GetTempPath()}\\{Guid.NewGuid().ToString("N").Substring(0, 8)}.zip";
-               
-                Console.Clear(); Console.ForegroundColor = ConsoleColor.Green;
-                InstallLog("=======================================================================================================================");
-                InstallLog("                                                     INSTALLER                                                         ");
-                InstallLog("                                                                                                                       ");
-                InstallLog("                                    █████╗ ██████╗ ███████╗ █████╗     ███████╗ ██╗                                    ");
-                InstallLog("                                   ██╔══██╗██╔══██╗██╔════╝██╔══██╗    ██╔════╝███║                                    ");
-                InstallLog("                                   ███████║██████╔╝█████╗  ███████║    ███████╗╚██║                                    ");
-                InstallLog("                                   ██╔══██║██╔══██╗██╔══╝  ██╔══██║    ╚════██║ ██║                                    ");
-                InstallLog("                                   ██║  ██║██║  ██║███████╗██║  ██║    ███████║ ██║                                    ");
-                InstallLog("                                   ╚═╝  ╚═╝╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝    ╚══════╝ ╚═╝                                    ");
-                InstallLog("                               *____________________________________________________*                                  ");
-                InstallLog("                                                                                                                       ");
-                InstallLog("                                        A Spaced out installer for Area51                                              ");
-                InstallLog("                          The Developers's: Joshua, Maxie, PandaStudios, Pyro & Swordsith                              ");
-                InstallLog($"                        Client Website: https://outerspace.store/ | Client Version: {ClientVersion}                   ");
-                InstallLog("                                                                                                                       ");
-                InstallLog("=======================================================================================================================\n");
-                Console.ForegroundColor = ConsoleColor.White; InstallLog("Checking for update....");
+                using (var client = new WebClient { Headers = { "Accept: text/html, application/xhtml+xml, */*", "User-Agent: Mozilla/9.0 (compatible; MSIE 9.0; Windows NT 6.1; WOW64; SpacewareAPI/9.0)" } })
+                {
+                    if (VRChatFolder() == string.Empty | !vrchatPath.Contains("VRChat")) return 0;
+                    ClientVersion = FileVersionInfo.GetVersionInfo($"{vrchatPath}\\Area51\\DLL\\Area51.dll").FileVersion;
+                    ServerVersion = client.DownloadString($"{APILink}version");
+                    zipPath = $"{Path.GetTempPath()}\\{Guid.NewGuid().ToString("N").Substring(0, 8)}.zip";
 
-                //if server version equals version on disk dont download else download the lastest version!
-                if (ServerVersion == ClientVersion) return false;
-                InstallLog($"Downloading New Update: {ServerVersion}");
-                client.DownloadFile(new Uri($"{APILink}update/{File.ReadAllText("Authorization.json")}"), zipPath);
-                return true;
+                    Console.Clear(); Console.ForegroundColor = ConsoleColor.Green;
+                    InstallLog("=======================================================================================================================");
+                    InstallLog("                                                     INSTALLER                                                         ");
+                    InstallLog("                                                                                                                       ");
+                    InstallLog("                                    █████╗ ██████╗ ███████╗ █████╗     ███████╗ ██╗                                    ");
+                    InstallLog("                                   ██╔══██╗██╔══██╗██╔════╝██╔══██╗    ██╔════╝███║                                    ");
+                    InstallLog("                                   ███████║██████╔╝█████╗  ███████║    ███████╗╚██║                                    ");
+                    InstallLog("                                   ██╔══██║██╔══██╗██╔══╝  ██╔══██║    ╚════██║ ██║                                    ");
+                    InstallLog("                                   ██║  ██║██║  ██║███████╗██║  ██║    ███████║ ██║                                    ");
+                    InstallLog("                                   ╚═╝  ╚═╝╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝    ╚══════╝ ╚═╝                                    ");
+                    InstallLog("                               *____________________________________________________*                                  ");
+                    InstallLog("                                                                                                                       ");
+                    InstallLog("                                        A Spaced out installer for Area51                                              ");
+                    InstallLog("                          The Developers's: Joshua, Maxie, PandaStudios, Pyro & Swordsith                              ");
+                    InstallLog($"                        Client Website: https://outerspace.store/ | Client Version: {ClientVersion}                   ");
+                    InstallLog("                                                                                                                       ");
+                    InstallLog("=======================================================================================================================\n");
+                    Console.ForegroundColor = ConsoleColor.White; InstallLog("Checking for update....");
+
+                    //if server version equals version on disk dont download else download the lastest version!
+                    if (ServerVersion == ClientVersion) return 1;
+                    InstallLog($"Downloading New Update: {ServerVersion}");
+                    client.DownloadFile(new Uri($"{APILink}update/{File.ReadAllText("Authorization.json")}"), zipPath);
+                    return 3;
+                }
             }
+            catch (Exception ErrorOnInstall)
+            {
+                if (ErrorOnInstall.InnerException != null)
+                {
+                    return 2;
+                }
+            }
+            return 2;
         }
         /// <summary>
-        /// // Method: InitExtaction();
-        /// // Description: Initializes the extaction process. 
-        /// // Usage: DownloadWorker.InitExtaction();
+        /// Method: InitExtaction();
+        /// Description: Initializes the extaction process. 
+        /// Usage: DownloadWorker.InitExtaction();
         /// </summary>
         public bool InitExtaction()
         {
             try
             {
+                //if path is not empty install else return exception
                 if (zipPath == string.Empty)  return false;
                 InstallLog("Installing....", true);
                 System.IO.Compression.ZipFile.ExtractToDirectory(zipPath, $"{vrchatPath}");
                 return true;
             }
-            catch (Exception InstallError)
+            catch (Exception ErrorOnExtaction)
             {
-                if(InstallError.InnerException != null)
+                if(ErrorOnExtaction.InnerException != null)
                 {
-                    InstallLog("Something really done goofed on install, please join server and make a ticket.\nDiscord: https://discord.gg/Paul", true);
+                    InstallLog("Something really done goofed on Extaction, please join server and make a ticket.\nDiscord: https://discord.gg/Paul", true);
                     return false;
                 }
             }
@@ -76,7 +89,7 @@ namespace Spaceware
         #region Public Functions / Variables - Log, APILink, VRChatpath ect. these are gloabally called throughout this project.
         public static void InstallLog(string text, bool state = false) { Console.WriteLine(text); if (state == true) { Console.Read(); } }
         public string VRChatFolder() { using (FolderBrowserDialog fdb = new FolderBrowserDialog()) { if (fdb.ShowDialog() == DialogResult.OK) vrchatPath = fdb.SelectedPath; return fdb.SelectedPath; } }
-        public string APILink = "https://api.outerspace.store/session/download/";
+        public readonly string APILink = "https://api.outerspace.store/session/download/";
         public string zipPath, vrchatPath, ClientVersion, ServerVersion = string.Empty;
         #endregion
 
--- Spaceware.cs
+++ Spaceware.cs
@@ -6,9 +6,9 @@ namespace Spaceware
     class Spaceware
     {
         /// <summary>
-        /// // Project Name: Main
-        /// // Description: Area51-Installer {Server AUTH: api.outerspace.store}
-        /// // Project Developer's: Installer: Josh(UrFingPoor) Serverside:  Maxie, PandaStudios 
+        /// Project Name: Main
+        /// Description: Area51-Installer {Server AUTH: api.outerspace.store}
+        /// Project Developer's: Installer: Josh(UrFingPoor) Serverside:  Maxie, PandaStudios 
         /// </summary>
         [STAThread]
         public static void Main(string[] args)
@@ -17,24 +17,30 @@ namespace Spaceware
 
             if (!File.Exists("Authorization.json"))
             {
-                DownloadAPI.InstallLog("Enter Token: ");
+                DownloadAPI.InstallLog("Enter Token: "); 
                 string token = Console.ReadLine();
                 File.AppendAllText("Authorization.json", token);
             }
 
             switch (Installer.InitDownload())
             {
-                case true:
+                case 0:
+                    Console.Clear();
+                    DownloadAPI.InstallLog("Please Make Sure To Select The VRChat Folder Anything, Else Will Give This Message.", true);
+                    break;
+                case 1:
+                    DownloadAPI.InstallLog("You seem to be on the lastest version, no need to update!", true);
+                    break;
+                case 2:
+                    DownloadAPI.InstallLog("Something really done goofed on install, please join server and make a ticket.\nDiscord: https://discord.gg/Paul", true);
+                    break;
+                case 3:
                     DownloadAPI.InstallLog($"Download Complete!, Press enter to install.", true);
                     if (Installer.InitExtaction())
                     {
                         DownloadAPI.InstallLog($"Install Complete!, Press enter to exit.", true);
                         File.Delete(Installer.zipPath);
-                        break;
                     }
-                    break;
-                case false:
-                    DownloadAPI.InstallLog("You seem to be on the lastest version, no need to update!", true);
                     break;
             }
         }
